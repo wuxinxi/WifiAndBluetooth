20988a46c1b6b3271b23e1ecd693a3704af8aaea
TangRen <996489865@qq.com>
2019-07-30 09:24:55 +0800

    新增数据转Excel、读取Excel

---
+++ app/src/main/java/com/study/file/entity/PeopleEntity.java
@@ -0,0 +1,15 @@
+package com.study.file.entity;
+
+/**
+ * 作者: TangRen on 2019/7/30
+ * 包名：com.study.file.entity
+ * 邮箱：996489865@qq.com
+ * TODO:一句话描述
+ */
+public class PeopleEntity {
+    public Long id;
+    public String userName;
+    public int userAge;
+    public String userIDCard;
+    public String empNo;
+}
